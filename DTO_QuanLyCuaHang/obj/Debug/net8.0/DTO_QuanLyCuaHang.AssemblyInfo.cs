7975a18eadf582c2a1467f55a3304419cedd81fd
thoanbt01636 <thaic8499@gmail.com>
2025-07-21 08:23:02 +0700

    thoan

--- DTO_QuanLyCuaHang/obj/Debug/net8.0/DTO_QuanLyCuaHang.AssemblyInfo.cs
+++ DTO_QuanLyCuaHang/obj/Debug/net8.0/DTO_QuanLyCuaHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTO_QuanLyCuaHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfb3521fbc952976db0437a7e74a95751e9ff01a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a2b93a2faa7a0db3aee148d1e4b478511351e6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTO_QuanLyCuaHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTO_QuanLyCuaHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
